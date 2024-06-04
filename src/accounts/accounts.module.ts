f2f064c4dbb2b8a3f1bded89f4572727af0f8db4
tariktrokic <tariktrokic@gmail.com>
2024-06-04 15:15:59 +0300

    Typo fix, switch setting to account

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
-import { Setting } from '../models/setting.model';
 import { AuthGuard } from '../common/guards/auth.guard';
+import { Account } from 'src/models/account.model';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Setting])],
+  imports: [SequelizeModule.forFeature([Account])],
   providers: [AccountsService, AuthGuard],
   controllers: [AccountsController],
 })
